263f9d01e90e148a1c9903abb564aa73ae2e64e0
Santiago Siordia <santiago.gonzalez.siordia@gmail.com>
2022-03-02 09:29:41 -0600

    feat: navigation capabilities added

--- android/app/src/main/java/com/jobsity/MainActivity.java
+++ android/app/src/main/java/com/jobsity/MainActivity.java
@@ -1,4 +1,5 @@
 package com.jobsity;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 
@@ -11,5 +12,9 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Jobsity";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }

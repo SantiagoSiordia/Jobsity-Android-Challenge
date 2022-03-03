94929b3dc420f2a3b0ca5fc6d620ca6350279616
Santiago Siordia <santiago.gonzalez.siordia@gmail.com>
2022-03-03 11:10:19 -0600

    feat: PIN, formatting on save, touch ID support

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import TouchID from 'react-native-touch-id';
-import { Provider } from 'react-redux';
-import { Navigator, store } from "./src/services"
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { LogBox } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
+import { Navigator, store } from './src/services';
 
 LogBox.ignoreLogs(['Require cycle:']);
-
-TouchID.authenticate('to demo this react-native component')
-.then(success => {
-  console.log(success);
-})
-.catch(error => {
-  console.log(error)
-});
 
 const queryClient = new QueryClient();
 

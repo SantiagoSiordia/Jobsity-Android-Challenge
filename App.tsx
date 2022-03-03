48b1973b24e7cdc884e85e6aca0ff5942aac56a1
Santiago Siordia <santiago.gonzalez.siordia@gmail.com>
2022-03-03 09:42:41 -0600

    refactor: Navigator in own file

--- App.tsx
+++ App.tsx
@@ -1,74 +1,30 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import {
-  SearchHeader,
-  Search,
-  SCREENS,
-  SeriesHeader,
-  SeriesList,
-  ShowDetails,
-  ShowDetailsHeader,
-  Image,
-  Episode,
-} from './src/screens';
+import TouchID from 'react-native-touch-id';
 
 const Stack = createNativeStackNavigator();
 const queryClient = new QueryClient();
 import { Provider } from 'react-redux';
-import { store } from './src/services/redux';
+import { Navigator, store } from "./src/services"
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { LogBox } from 'react-native';
 
 LogBox.ignoreLogs(['Require cycle:']);
 
+TouchID.authenticate('to demo this react-native component')
+  .then(success => {
+    console.log(success);
+  })
+  .catch(error => {
+    console.log(error)
+  });
+
 const App = () => {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              animation: 'fade',
-            }}>
-            <Stack.Screen
-              name={SCREENS.SERIES_LIST}
-              component={SeriesList}
-              options={{
-                header: headerProps => <SeriesHeader {...headerProps} />,
-              }}
-            />
-            <Stack.Screen
-              name={SCREENS.SEARCH_SHOWS}
-              component={Search}
-              options={{
-                header: headerProps => <SearchHeader {...headerProps} />,
-              }}
-            />
-            <Stack.Screen
-              name={SCREENS.SHOW_DETAILS}
-              component={ShowDetails}
-              options={{
-                header: headerProps => <ShowDetailsHeader {...headerProps} />,
-              }}
-            />
-            <Stack.Screen
-              name={SCREENS.IMAGE}
-              component={Image}
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen
-              name={SCREENS.EPISODE_DETAILS}
-              component={Episode}
-              options={{
-                header: headerProps => <ShowDetailsHeader {...headerProps} />,
-              }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <Navigator />
       </QueryClientProvider>
     </Provider>
   );
---
+++ src/services/navigation/Navigator.tsx
@@ -0,0 +1,52 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Episode, Image, SCREENS, Search, SearchHeader, SeriesHeader, SeriesList, ShowDetails, ShowDetailsHeader } from "@screens";
+import React from "react";
+import { FC } from "react";
+
+const Stack = createNativeStackNavigator();
+
+export const Navigator: FC = () => {
+    return <NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{
+        animation: 'fade',
+      }}>
+      <Stack.Screen
+        name={SCREENS.SERIES_LIST}
+        component={SeriesList}
+        options={{
+          header: headerProps => <SeriesHeader {...headerProps} />,
+        }}
+      />
+      <Stack.Screen
+        name={SCREENS.SEARCH_SHOWS}
+        component={Search}
+        options={{
+          header: headerProps => <SearchHeader {...headerProps} />,
+        }}
+      />
+      <Stack.Screen
+        name={SCREENS.SHOW_DETAILS}
+        component={ShowDetails}
+        options={{
+          header: headerProps => <ShowDetailsHeader {...headerProps} />,
+        }}
+      />
+      <Stack.Screen
+        name={SCREENS.IMAGE}
+        component={Image}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name={SCREENS.EPISODE_DETAILS}
+        component={Episode}
+        options={{
+          header: headerProps => <ShowDetailsHeader {...headerProps} />,
+        }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+}

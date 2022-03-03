0d1c03c4cc9dbbebb6308f36d65d4a029074eed0
Santiago Siordia <santiago.gonzalez.siordia@gmail.com>
2022-03-02 23:55:36 -0600

    deletion: unused imports

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import {
   SearchHeader,

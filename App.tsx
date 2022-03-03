26693ec6bd3bb26538fd563b5566a926929626c0
Santiago Siordia <santiago.gonzalez.siordia@gmail.com>
2022-03-03 09:44:01 -0600

    refactor: App.tsx formatted

--- App.tsx
+++ App.tsx
@@ -1,10 +1,6 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import TouchID from 'react-native-touch-id';
-
-const Stack = createNativeStackNavigator();
-const queryClient = new QueryClient();
 import { Provider } from 'react-redux';
 import { Navigator, store } from "./src/services"
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -13,27 +9,25 @@ import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['Require cycle:']);
 
 TouchID.authenticate('to demo this react-native component')
-  .then(success => {
-    console.log(success);
-  })
-  .catch(error => {
-    console.log(error)
-  });
+.then(success => {
+  console.log(success);
+})
+.catch(error => {
+  console.log(error)
+});
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Navigator />
-      </QueryClientProvider>
-    </Provider>
-  );
-};
-
-export default () => {
-  return (
     <SafeAreaProvider>
-      <App />
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <Navigator />
+        </QueryClientProvider>
+      </Provider>
     </SafeAreaProvider>
   );
 };
+
+export default App;
